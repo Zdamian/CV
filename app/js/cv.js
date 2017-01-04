6dfeebb118cc092eff3e774927097a19be69da04
Zdamian <damianzielin.js@gmail.com>
2017-01-04 10:57:24 +0100

    modified skills section

--- app/js/cv.js
+++ app/js/cv.js
@@ -79,63 +79,60 @@ $(function() {
         $(this).tab('show');
     });
 
-    $("#gimp").circliful({
-        animationStep: 5,
-        foregroundBorderWidth: 15,
-        backgroundBorderWidth: 15,
-        percent: 50,
-        iconPosition: 'middle',
-        text: 'Gimp',
-        textBelow: true
+    $('.pie_progress').asPieProgress({
+        namespace: 'pie_progress'
+    });
+    // Example with grater loading time - loads longer
+    $('.pie_progress--slow').asPieProgress({
+        namespace: 'pie_progress',
+        goal: 1000,
+        min: 0,
+        max: 1000,
+        speed: 200,
+        easing: 'linear'
+    });
+    // Example with grater loading time - loads longer
+    $('.pie_progress--countdown').asPieProgress({
+        namespace: 'pie_progress',
+        easing: 'linear',
+        first: 120,
+        max: 120,
+        goal: 0,
+        speed: 1200, // 120 s * 1000 ms per s / 100
+        numberCallback: function(n) {
+            var minutes = Math.floor(this.now / 60);
+            var seconds = this.now % 60;
+            if (seconds < 10) {
+                seconds = '0' + seconds;
+            }
+            return minutes + ': ' + seconds;
+        }
     });
 
-    $("#blender").circliful({
-        animationStep: 5,
-        foregroundBorderWidth: 15,
-        backgroundBorderWidth: 15,
-        percent: 60,
-        iconPosition: 'middle',
-        text: 'Blender',
-        textBelow: true
-    });
+    var $animation_elements = $('.skill');
+    var $window = $(window);
 
-    $("#html").circliful({
-        animationStep: 5,
-        foregroundBorderWidth: 15,
-        backgroundBorderWidth: 15,
-        percent: 60,
-        iconPosition: 'middle',
-        text: 'HTML',
-        textBelow: true
-    });
+    function check_if_in_view() {
+        var window_height = $window.height();
+        var window_top_position = $window.scrollTop();
+        var window_bottom_position = (window_top_position + window_height);
 
-    $("#css").circliful({
-        animationStep: 5,
-        foregroundBorderWidth: 15,
-        backgroundBorderWidth: 15,
-        percent: 60,
-        iconPosition: 'middle',
-        text: 'CSS',
-        textBelow: true
-    });
+        $.each($animation_elements, function() {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
 
-    $("#javascript").circliful({
-        animationStep: 5,
-        foregroundBorderWidth: 15,
-        backgroundBorderWidth: 15,
-        percent: 40,
-        iconPosition: 'middle',
-        text: 'JavaScript',
-        textBelow: true
-    });
+            //check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+                $('.pie_progress').asPieProgress('start');
+            } else {
+                $('.pie_progress').asPieProgress('stop');
+            }
+        });
+    }
 
-    $("#jquery").circliful({
-        animationStep: 5,
-        foregroundBorderWidth: 15,
-        backgroundBorderWidth: 15,
-        percent: 50,
-        iconPosition: 'middle',
-        text: 'jQuery',
-        textBelow: true
-    });
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
 });
